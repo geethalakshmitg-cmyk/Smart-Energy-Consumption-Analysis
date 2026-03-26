052bba2df180817e2dad9e06d44c7c9fb82dd620
geethalakshmitg-cmyk <geethalakshmi.tg@gmail.com>
2026-03-26 19:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import Milestone4
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My project is running!"
+    result = Milestone4.get_output()
+    return f"<h1>{result}</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
